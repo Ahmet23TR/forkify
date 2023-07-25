0bdf0da2df37f4185b89d9709b2551cc144f940e
Ahmet23TR <eminguclu@marun.edu.tr>
2023-07-25 21:22:23 +0300

    debug6

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 // import { Fraction } from 'fractional';
-
+import fracty from 'fracty';
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMessage = 'We could not find that recipe. Please try another one!';
@@ -127,7 +127,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? fracty(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
