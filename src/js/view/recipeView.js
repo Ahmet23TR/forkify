e520e1ff6ba883993005d7b3b6a6404e54debdb1
Ahmet23TR <eminguclu@marun.edu.tr>
2023-07-25 18:14:27 +0300

    debugging

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -3,6 +3,7 @@ import View from './View.js';
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 import { Fraction } from 'fractional';
+import { numberToFraction } from '../helpers.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
