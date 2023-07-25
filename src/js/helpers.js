83b885a93207f34127049730fd864f67ac99d803
Ahmet23TR <eminguclu@marun.edu.tr>
2023-07-25 21:16:40 +0300

    debug5

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -36,37 +36,7 @@ export const AJAX = async function (url, uploadData = undefined) {
  * - 1.25 to 1 1/4
  * - 2 to 2
  */
-export const numberToFraction = function (amount) {
-  // This is a whole number and doesn't need modification.
-  if (parseFloat(amount) === parseInt(amount)) {
-    return amount;
-  }
-  // Next 12 lines are cribbed from https://stackoverflow.com/a/23575406.
-  var gcd = function (a, b) {
-    if (b < 0.0000001) {
-      return a;
-    }
-    return gcd(b, Math.floor(a % b));
-  };
-  var len = amount.toString().length - 2;
-  var denominator = Math.pow(10, len);
-  var numerator = amount * denominator;
-  var divisor = gcd(numerator, denominator);
-  numerator /= divisor;
-  denominator /= divisor;
-  var base = 0;
-  // In a scenario like 3/2, convert to 1 1/2
-  // by pulling out the base number and reducing the numerator.
-  if (numerator > denominator) {
-    base = Math.floor(numerator / denominator);
-    numerator -= base * denominator;
-  }
-  amount = Math.floor(numerator) + '/' + Math.floor(denominator);
-  if (base) {
-    amount = base + ' ' + amount;
-  }
-  return amount;
-};
+
 /*
 export const getJSON = async function (url) {
   try {
--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -2,7 +2,6 @@ import View from './View.js';
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 // import { Fraction } from 'fractional';
-import { numberToFraction } from '../helpers.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
