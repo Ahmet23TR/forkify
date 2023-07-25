25d2f0e5a5a9e8fd7a7687df990ac29191d6cbd8
Ahmet23TR <eminguclu@marun.edu.tr>
2023-07-25 17:40:19 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 1;
+export const RES_PER_PAGE = 10;
+export const KEY = '4b4bd217-c519-47d0-8723-808963a850b7';
+export const MODAL_CLOSE_SEC = 2.5;
